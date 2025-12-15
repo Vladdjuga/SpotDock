aadd98312b34d76b38760f42f15c276f04ec0dee
Vladdjuga <vladdjuga@gmail.com>
2025-12-15 20:13:02 +0200

    feat: Integrate MassTransit with RabbitMQ for messaging; update DI and configuration settings

--- SpotDock.Web/Program.cs
+++ SpotDock.Web/Program.cs
@@ -1,3 +1,4 @@
+using MassTransit;
 using SpotDock.Modules.Billing.Infrastructure.DI;
 using SpotDock.Modules.Market.Infrastructure.DI;
 using SpotDock.Modules.Compute.Infrastructure.DI;
@@ -6,13 +7,33 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
 
-var app = builder.Build();
-
-// Add DI
+// Add DI for modules
 builder.Services.AddMarketModule(builder.Configuration);
 builder.Services.AddComputeModule(builder.Configuration);
-// builder.Services.AddKernelModule(app.Configuration);
-builder.Services.AddBillingModule(app.Configuration);
+builder.Services.AddBillingModule(builder.Configuration);
+
+// MassTransit + RabbitMQ
+builder.Services.AddMassTransit(cfg =>
+{
+    // TODO: register consumers/sagas from modules when they appear
+
+    cfg.UsingRabbitMq((context, busCfg) =>
+    {
+        var rabbitSection = builder.Configuration.GetSection("RabbitMq");
+        var host = rabbitSection.GetValue<string>("Host") ?? "localhost";
+        var virtualHost = rabbitSection.GetValue<string>("VirtualHost") ?? "/";
+        var username = rabbitSection.GetValue<string>("Username") ?? "guest";
+        var password = rabbitSection.GetValue<string>("Password") ?? "guest";
+
+        busCfg.Host(host, virtualHost, h =>
+        {
+            h.Username(username);
+            h.Password(password);
+        });
+    });
+});
+
+var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
